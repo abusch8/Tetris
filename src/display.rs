b86065562de402f6be3362fbe59d758c48f71f70
abusch8 <abusch8@huskers.unl.edu>
2024-06-07 12:43:19 -0500

    Refactor

--- src/display.rs
+++ src/display.rs
@@ -11,8 +11,8 @@ pub const BOARD_DIMENSION: Dimension = (10, 20);
 
 pub type Dimension = (i32, i32);
 
-const WIDTH: u16 = BOARD_DIMENSION.0 as u16 * 2 + 2;
-const HEIGHT: u16 = BOARD_DIMENSION.1 as u16 + 2;
+const BOARD_WIDTH: u16 = BOARD_DIMENSION.0 as u16 * 2 + 2;
+const BOARD_HEIGHT: u16 = BOARD_DIMENSION.1 as u16 + 2;
 
 pub fn render(game: &Game) -> Result<()> {
     let mut stdout = stdout();
@@ -21,18 +21,15 @@ pub fn render(game: &Game) -> Result<()> {
 
     let (terminal_width, _terminal_height) = terminal::size().unwrap();
 
-    let board_width_start = terminal_width / 2 - WIDTH / 2;
-    let board_width_end = board_width_start + WIDTH;
-
-    let board_height_start = 1 as u16;
-    let board_height_end = HEIGHT - 1;
+    let board_width_start = terminal_width / 2 - BOARD_WIDTH / 2;
+    let board_width_end = board_width_start + BOARD_WIDTH;
 
     fn position_in_view(position: &Dimension, view: &Dimension, offset_x: i32) -> bool {
         BOARD_DIMENSION.1 - position.1 == view.1 && ((position.0 + 1) * 2 + offset_x == view.0 || (position.0 + 1) * 2 + offset_x - 1 == view.0)
     }
 
     for x in board_width_start + 1..board_width_end - 1 {
-        for y in board_height_start..board_height_end {
+        for y in 1..BOARD_HEIGHT - 1 {
             stdout
                 .queue(MoveTo(x as u16, y as u16))?
                 .queue(PrintStyledContent((|| {
@@ -65,9 +62,9 @@ pub fn render(game: &Game) -> Result<()> {
             .queue(Print("        "))?;
         for position in tetromino.shape.iter().map(|(x, y)| (*x as u16, *y as u16)) {
             stdout
-                .queue(MoveTo((position.0 - 3) * 2 + board_width_end + 2, HEIGHT - position.1 + 1 + (i as u16 * 3)))?
+                .queue(MoveTo((position.0 - 3) * 2 + board_width_end + 2, BOARD_HEIGHT - position.1 + 1 + (i as u16 * 3)))?
                 .queue(PrintStyledContent(" ".on(tetromino.color)))?
-                .queue(MoveTo((position.0 - 3) * 2 + board_width_end + 1, HEIGHT - position.1 + 1 + (i as u16 * 3)))?
+                .queue(MoveTo((position.0 - 3) * 2 + board_width_end + 1, BOARD_HEIGHT - position.1 + 1 + (i as u16 * 3)))?
                 .queue(PrintStyledContent(" ".on(tetromino.color)))?;
         }
     }
@@ -79,9 +76,9 @@ pub fn render(game: &Game) -> Result<()> {
             .queue(Print("        "))?;
         for position in holding.shape.iter().map(|(x, y)| (*x as u16, *y as u16)) {
             stdout
-                .queue(MoveTo((position.0 - 3) * 2 + board_width_start - 9, HEIGHT - position.1 + 1))?
+                .queue(MoveTo((position.0 - 3) * 2 + board_width_start - 9, BOARD_HEIGHT - position.1 + 1))?
                 .queue(PrintStyledContent(" ".on(holding.color)))?
-                .queue(MoveTo((position.0 - 3) * 2 + board_width_start - 8, HEIGHT - position.1 + 1))?
+                .queue(MoveTo((position.0 - 3) * 2 + board_width_start - 8, BOARD_HEIGHT - position.1 + 1))?
                 .queue(PrintStyledContent(" ".on(holding.color)))?;
         }
     }
@@ -108,27 +105,27 @@ pub fn draw(game: &Game) -> Result<()> {
     debug_println!("REDRAW");
     debug_println!("{}", terminal_width);
 
-    let board_width_start = terminal_width / 2 - WIDTH / 2;
-    let board_width_end = board_width_start + WIDTH;
+    let board_width_start = terminal_width / 2 - BOARD_WIDTH / 2;
+    let board_width_end = board_width_start + BOARD_WIDTH;
 
     debug_println!("{}", board_width_start);
 
     for x in board_width_start..board_width_end {
-        for y in 0..HEIGHT {
+        for y in 0..BOARD_HEIGHT {
             stdout
                 .queue(MoveTo(x, y))?
                 .queue(Print(
                     if x == board_width_start && y == 0 {
                         "╔"
-                    } else if x == board_width_start && y == HEIGHT - 1 {
+                    } else if x == board_width_start && y == BOARD_HEIGHT - 1 {
                         "╚"
                     } else if x == board_width_end - 1 && y == 0 {
                         "╗"
-                    } else if x == board_width_end - 1 && y == HEIGHT - 1 {
+                    } else if x == board_width_end - 1 && y == BOARD_HEIGHT - 1 {
                         "╝"
                     } else if x == board_width_start || x == board_width_end - 1 {
                         "║"
-                    } else if y == 0 || y == HEIGHT - 1 {
+                    } else if y == 0 || y == BOARD_HEIGHT - 1 {
                         "═"
                     } else if x % 2 != terminal_width / 2 % 2 {
                         "."
